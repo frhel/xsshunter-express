0749be5ab1644ad186c8f3bd8ee404f580195675
frhel <freyr.helga@gmail.com>
2025-06-27 22:09:30 +0200

    Add Disable SSL Option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-echo "Initializing SSL/TLS..."
-# Set up Greenlock
-# Test if --maintainer-email is required, we can set it via environment variables...
-npx greenlock init --config-dir /app/greenlock.d --maintainer-email $SSL_CONTACT_EMAIL
-npx greenlock add --subject $HOSTNAME --altnames "$HOSTNAME"
+if [[ $SSL_ENABLED == "true" ]]; then
+	echo "Initializing SSL/TLS..."
+	# Set up Greenlock
+	# Test if --maintainer-email is required, we can set it via environment variables...
+	npx greenlock init --config-dir /app/greenlock.d --maintainer-email $SSL_CONTACT_EMAIL
+	npx greenlock add --subject $HOSTNAME --altnames "$HOSTNAME"
+else
+	echo "SSL/TLS is disabled, skipping SSL/TLS setup..."
+fi
 
 echo "Starting server..."
 node server.js
